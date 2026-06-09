2bce34485409c841ab2a7383356c46029f7454b0
satyanshu123456 <satyanshugautam6@gmail.com>
2026-06-09 20:15:26 +0530

    Refactor code structure for improved readability and maintainability

--- Projects/Cravings/src/App.jsx
+++ Projects/Cravings/src/App.jsx
@@ -1,16 +1,33 @@
+import bgImage from "./assets/images/HelpPage.jpg";
+
 const App = () => {
   return (
     <>
-      <div className="bg-green-200 text-amber-800 p-3.25">App</div>
+      <div className="bg-green-200 text-amber-800 p-3.25 flex justify-between">
+        <div className="text-center font-bold">Cravings</div>
+        <div className="flex gap-2.5">
+          <div className="flex justify-center align-center ">
+            <button className="btn btn-primary  bg-amber-700 h-8 w-20 text-center text-white rounded-2xl hover:bg-amber-400 active:bg-amber-500">
+              Login
+            </button>
+          </div>
+          <div className="flex justify-center align-center ">
+            <button className="btn btn-primary  bg-amber-700 h-8 w-20 text-center text-white rounded-2xl  hover:bg-amber-500 active:bg-amber-500">
+              Register
+            </button>
+          </div>
+        </div>
+      </div>
 
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa sed
-        libero eligendi. Facilis nam minima alias architecto repudiandae, harum
-        consequatur! Odio et ducimus quis distinctio illo dolores voluptatum
-        accusantium doloribus.
-      </p>
+      <div class="w-full h-screen bg-[url('./assets/images/HelpPage.jpg')] bg-cover bg-center">
+      
+      </div>
 
-      <div className="flex justify-center align-center "><button className="btn btn-primary  bg-blue-700 h-10 w-25 text-center text-white rounded-2xl">Submit</button></div>
+      <div className="flex justify-center align-center m-2">
+        <button className="btn btn-primary  bg-blue-700 h-10 w-25 text-center text-white rounded-2xl">
+          Submit
+        </button>
+      </div>
     </>
   );
 };
